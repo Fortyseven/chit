abaa6cbd67d7a6734484964b1d37ddaed9906578
Fortyseven <fortyseven@network47.org>
2024-03-02 11:52:35 -0500

    Adds rewind-to-index for timeline

--- src/lib/chat.js
+++ src/lib/chat.js
@@ -2,23 +2,27 @@ import { chatTimeline } from '../stores/stores';
 import { OL_chat } from '../lib/api';
 import { get } from 'svelte/store';
 
-export async function rerollLastResponse() {
-    // check if last entry in chatTimeline is a bot response
-    const last = get(chatTimeline)[get(chatTimeline).length - 1];
-    console.log(last);
-    if (last.role !== 'user') {
-        console.log('Last message is user message, rerolling');
-        console.warn('Timeline A: ', get(chatTimeline));
-        // remove last entry
-        // let timeline = get(chatTimeline);
-        // timeline.pop();
-        // chatTimeline.set(timeline);
-        // chatTimeline.update((timeline) => {
-        //     timeline.pop();
-        //     return timeline;
-        // });
+/**
+ * Truncates the up to the given index in the chat timeline.
+ * @param {*} index
+ */
+export function rewindChatToIndex(index) {
+    chatTimeline.update((timeline) => {
+        return timeline.slice(0, index + 1);
+    });
+}
+
+export async function rerollLastResponse(index = undefined) {
+    if (index) {
+        // we're rerolling back to a specific response
+        rewindChatToIndex(index);
+    }
+
+    // check if previous entry in chatTimeline is a bot response
+    const priorChatEntry = get(chatTimeline)[get(chatTimeline).length - 1];
+
+    if (priorChatEntry.role !== 'user') {
         popLastMessage();
-        console.warn('Timeline B: ', get(chatTimeline));
 
         // rerun inference
         let response = await OL_chat();
@@ -29,8 +33,6 @@ export async function rerollLastResponse() {
                 timeline.push(response.message);
                 return timeline;
             });
-
-            console.warn('Timeline C: ', get(chatTimeline));
         } else {
             console.error('Failed to reroll last response');
         }
