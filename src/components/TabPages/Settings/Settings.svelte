591027ab3b3a62d4d355696b2350d5be08ee1ade
Fortyseven <fortyseven@network47.org>
2024-03-16 02:28:48 -0400

    Remove import/export for now

--- src/components/TabPages/Settings/Settings.svelte
+++ src/components/TabPages/Settings/Settings.svelte
@@ -1,12 +1,12 @@
 <script>
     import Settings__General from './Settings__General.svelte';
-    import Settings__ImportExport from './Settings__ImportExport.svelte';
+    // import Settings__ImportExport from './Settings__ImportExport.svelte';
     import Settings__UI from './Settings__UI.svelte';
 </script>
 
 <div class="settings">
     <Settings__General />
-    <Settings__ImportExport />
+    <!-- <Settings__ImportExport /> -->
     <Settings__UI />
     <div class="note">
         Double click on field labels to reset to default values where
