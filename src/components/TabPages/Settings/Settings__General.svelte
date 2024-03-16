13284447b2840a29bb57c11533333b7102fd970c
Fortyseven <fortyseven@network47.org>
2024-03-16 02:28:13 -0400

    Some Settings cleanup

--- src/components/TabPages/Settings/Settings__General.svelte
+++ src/components/TabPages/Settings/Settings__General.svelte
@@ -4,70 +4,39 @@
         DEFAULT_OL_ENDPOINT,
         DEFAULT_PB_ENDPOINT
     } from '$stores/stores';
+    import SettingControl from './UI/SettingControl.svelte';
+
+    $: pbAdminLink = new URL('_/', $appState.pbEndpoint);
 </script>
 
 <fieldset>
     <legend>General</legend>
     <div>
-        <div>
-            <label
-                for="api-endpoint"
-                on:dblclick={() => {
-                    $appState.apiEndpoint = DEFAULT_OL_ENDPOINT;
-                }}
-            >
-                API Endpoint
-            </label>
-            <input
-                type="text"
-                bind:value={$appState.apiEndpoint}
-                placeholder="API Endpoint..."
-            />
-        </div>
-        <div>
-            <label
-                for="pb-endpoint"
-                on:dblclick={() => {
-                    $appState.pbEndpoint = DEFAULT_PB_ENDPOINT;
-                }}
-            >
-                PocketBase Endpoint <small>(Optional)</small>
-            </label>
-            <input
-                type="text"
-                bind:value={$appState.pbEndpoint}
-                placeholder="API Endpoint..."
-            />
-        </div>
+        <SettingControl
+            defaultValue={DEFAULT_OL_ENDPOINT}
+            bind:value={$appState.apiEndpoint}
+            label="API Endpoint"
+            placeholder="API Endpoint..."
+        />
+        <SettingControl
+            defaultValue={DEFAULT_PB_ENDPOINT}
+            bind:value={$appState.pbEndpoint}
+            label="PocketBase Endpoint"
+            placeholder="API Endpoint..."
+            supplementalUrl={pbAdminLink}
+            supplementalUrlName="Admin"
+        />
     </div>
 </fieldset>
 
 <style lang="scss">
     fieldset {
         > div {
-            display: flex;
-            flex-direction: column;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
             margin-bottom: 1em;
+            flex-wrap: wrap;
             gap: 1em;
-            > div {
-                display: flex;
-
-                label {
-                    display: block;
-                    flex: 0 0 300px;
-                    margin-bottom: 0.5em;
-                    padding: 0;
-                }
-
-                input {
-                    flex: auto;
-                    display: block;
-                    padding: 0.5em;
-                    border-radius: 5px;
-                    border: 1px solid var(--color-primary);
-                    font-family: monospace;
-                }
-            }
         }
     }
 </style>
---
+++ src/components/TabPages/Settings/UI/SettingControl.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let defaultValue;
+    export let value;
+    export let label;
+    export let supplementalUrl;
+    export let supplementalUrlName;
+    export let placeholder;
+</script>
+
+<div>
+    <label
+        for="pb-endpoint"
+        on:dblclick={() => {
+            value = defaultValue;
+        }}
+    >
+        {label}
+    </label>
+    <input type="text" bind:value {placeholder} />
+
+    {#if supplementalUrl}
+        <a href={supplementalUrl} target="_blank">{supplementalUrlName}</a>
+    {/if}
+</div>
+
+<style lang="scss">
+    div {
+        width: 100%;
+        display: flex;
+        align-items: center;
+
+        label {
+            flex: 0 0 auto;
+            padding: 0;
+            padding-right: 1em;
+            text-align: right;
+        }
+
+        input {
+            flex: auto;
+            display: block;
+            padding: 0.5em;
+            border-radius: 5px;
+            border: 1px solid var(--color-primary);
+            font-family: monospace;
+        }
+
+        a {
+            text-decoration: underline;
+            display: block;
+            padding-left: 1em;
+            color: lightblue;
+        }
+    }
+</style>
