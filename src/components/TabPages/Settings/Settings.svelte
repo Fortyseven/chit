394126037ca78cb32860c5c9ce2003ee4661fe94
Fortyseven <fortyseven@network47.org>
2024-05-02 12:55:02 -0400

    feat: adds footer to settings/general with links to the github, etc.

--- src/components/TabPages/Settings/Settings.svelte
+++ src/components/TabPages/Settings/Settings.svelte
@@ -1,17 +1,25 @@
 <script>
     import Settings__General from './Settings__General.svelte';
     import Settings__Appearance from './Settings__Appearance.svelte';
+    import Settings__Footer from './Settings__Footer.svelte';
 </script>
 
-<div class="settings">
-    <h1 class="text-primary pb-4 text-3xl">Settings</h1>
-
-    <Settings__General />
-    <!-- <Settings__ImportExport /> -->
-    <Settings__Appearance />
-    <div class="note">
-        Double click on field labels to reset to default values where
-        applicable. (This is true throughout the app.)
+<div class="settings h-full">
+    <div class="flex flex-col h-full">
+        <div><h1 class="text-primary pb-4 text-3xl">Settings</h1></div>
+        <div class="flex-none">
+            <Settings__General />
+        </div>
+        <div class="flex-none">
+            <Settings__Appearance />
+        </div>
+        <div class="note flex-grow">
+            Double click on field labels to reset to default values where
+            applicable. (This is true throughout the app.)
+        </div>
+        <div clss="flex-none">
+            <Settings__Footer />
+        </div>
     </div>
 </div>
 
---
+++ src/components/TabPages/Settings/Settings__Footer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Button from '$components/UI/Button.svelte';
+</script>
+
+<footer>
+    <fieldset>
+        <legend>Links</legend>
+        <ul class="flex">
+            <li>
+                <Button>
+                    <a href="https://github.com/Fortyseven/chit">GitHub</a>
+                </Button>
+            </li>
+            <li>
+                <Button>
+                    <a href="https://Network47.org">Network 47</a>
+                </Button>
+            </li>
+            <li>
+                <Button>
+                    <a href="https://defcon.social/@fortyseven">Mastodon</a>
+                </Button>
+            </li>
+        </ul>
+    </fieldset>
+</footer>
