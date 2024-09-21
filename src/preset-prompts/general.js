891e170611915af5dd8eba00db03c31a7eca6415
Fortyseven <fortyseven@network47.org>
2024-09-21 17:10:58 -0400

    feat: adds a new 'general' prompt

---
+++ src/preset-prompts/general.js
@@ -0,0 +1,7 @@
+export default {
+    name: '💡 General Query',
+    prompt: `You are a general purpose AI information assistant. Your response should be brief, concise and to the point. Your initial response should not exceed a paragraph. Ask if the user would like to know more before getting detailed, where more information is available and relevant.
+
+IMPORTANT: Do NOT include opinion, interpretations, or infer additional context where it does not exist in factual data. Only use factual data; do not invent information. Strive for accuracy using ONLY factual information. This is true for responses, or for any follow-up questions asked by the user: only use factual data.
+`
+};
--- src/preset-prompts/index.js
+++ src/preset-prompts/index.js
@@ -1,19 +1,21 @@
-import analysis from "./analysis.js";
-import translate from "./translate.js";
-import sdprompt from "./sdprompt.js";
-import summarize from "./summarize.js";
-import extract_and_infer from "./extract-and-infer.js";
-import retort from "./retort.js";
-import eli5 from "./eli5.js";
+import analysis from './analysis.js';
+import translate from './translate.js';
+import sdprompt from './sdprompt.js';
+import summarize from './general.js';
+import summarize from './summarize.js';
+import extract_and_infer from './extract-and-infer.js';
+import retort from './retort.js';
+import eli5 from './eli5.js';
 
 const SYSTEM_PROMPTS = {
+    general,
     summarize,
     eli5,
     translate,
     sdprompt,
     analysis,
     extract_and_infer,
-    retort,
+    retort
 };
 
 export default SYSTEM_PROMPTS;
